7eb02ccf94d9d9aed73119c82035f53d74988ce1
LorenzoCucchi <lorenzo.cucchi@skywarder.eu>
2024-03-30 23:52:02 +0100

    [MUTEX] added mutex to share I2C peripheral between sensor drivers
    Mutex created in the following way in main and used the same type inside
    the sensor drivers
    type Channel1 =
    Mutex<CriticalSectionRawMutex, Option<I2c<'static,
    peripherals::I2C1, NoDma, NoDma>>>;
    static I2C_REF: Channel1 = Mutex::new(None);
    
    Inside the sensor to use the peripheral you cant take as ref the
    MutexGuard.
    let mut i2c_unlocked = self.i2c.lock().await;
    let i2c_mut = i2c_unlocked.as_mut().ok_or(Error).unwrap();

--- src/lps22hh/mod.rs
+++ src/lps22hh/mod.rs
@@ -1,61 +1,47 @@
+use core::fmt::Error;
 use defmt::*;
-use embassy_stm32::time::Hertz;
-use embassy_stm32::{dma::NoDma, i2c, i2c::Instance, interrupt::typelevel::Binding, Peripheral};
+use embassy_stm32::{dma::NoDma, i2c::I2c, peripherals};
 mod reg;
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+use embassy_sync::mutex::Mutex;
 pub use reg::Register;
 
-use crate::lps22hh::reg::{IF_ADD_INC, LOW_NOISE_EN};
+type Channel1 =
+    Mutex<CriticalSectionRawMutex, Option<I2c<'static, peripherals::I2C1, NoDma, NoDma>>>;
 
-pub struct Lps22hh<'a, T: i2c::Instance> {
-    i2c: i2c::I2c<'a, T>,
+pub struct Lps22hh {
+    i2c: &'static Channel1,
 }
 
-impl<'a, T: i2c::Instance> Lps22hh<'a, T> {
+impl Lps22hh {
     #[must_use]
-    pub fn new(
-        peripheral: impl Peripheral<P = T> + 'a,
-        scl_pin: impl Peripheral<P = impl i2c::SclPin<T>> + 'a,
-        sda_pin: impl Peripheral<P = impl i2c::SdaPin<T>> + 'a,
-        irq: impl Binding<<T as Instance>::ErrorInterrupt, embassy_stm32::i2c::ErrorInterruptHandler<T>>
-            + Binding<<T as Instance>::EventInterrupt, embassy_stm32::i2c::EventInterruptHandler<T>>
-            + 'a,
-    ) -> Self {
-        Self {
-            i2c: i2c::I2c::new(
-                peripheral,
-                scl_pin,
-                sda_pin,
-                irq,
-                NoDma,
-                NoDma,
-                Hertz(100_000),
-                i2c::Config::default(),
-            ),
-        }
+    pub fn new(i2c: &'static Channel1) -> Self {
+        Self { i2c }
     }
 
-    pub fn check_device_id(&mut self) -> bool {
+    pub async fn check_device_id(&mut self) -> Result<bool, Error> {
         let mut data = [0u8, 1];
-        match self
-            .i2c
-            .blocking_write_read(reg::I2C_SAD, &[Register::WHO_AM_I as u8], &mut data)
-        {
+        let mut i2c_unlocked = self.i2c.lock().await;
+
+        let i2c_mut = i2c_unlocked.as_mut().ok_or(Error).unwrap(); // Lock the inner I2c instance
+
+        match i2c_mut.blocking_write_read(reg::I2C_SAD, &[Register::WHO_AM_I as u8], &mut data) {
             Ok(()) => {
                 info!("Whoami: {}", data[0]);
                 if data[0] == reg::DEVICE_ID {
-                    true
+                    Ok(true)
                 } else {
-                    false
+                    Ok(false)
                 }
             }
             Err(e) => {
                 error!("I2C Error: {:?}", e);
-                false
+                Err(Error)
             }
         }
     }
 
-    pub fn apply_config(&mut self) {
+    pub async fn apply_config(&mut self) -> Result<bool, Error> {
         let mut reg1: u8 = 0;
 
         // === CTRL_REG1 (10h) ===
@@ -64,10 +50,12 @@ impl<'a, T: i2c::Instance> Lps22hh<'a, T> {
         info!("CTRL_REG1 to write: {}", reg1);
 
         let mut data = [0u8, 1];
-        match self
-            .i2c
-            .blocking_write_read(reg::I2C_SAD, &[Register::CTRL_REG1 as u8], &mut data)
-        {
+        let mut i2c_unlocked = self.i2c.lock().await;
+
+        //let i2c_guard: &I2c<'static, peripherals::I2C1, NoDma, NoDma> = i2c_unlocked.as_ref().ok_or(Error)?;
+        let i2c_mut = i2c_unlocked.as_mut().ok_or(Error).unwrap(); // Lock the inner I2c instance
+
+        match i2c_mut.blocking_write_read(reg::I2C_SAD, &[Register::CTRL_REG1 as u8], &mut data) {
             Ok(()) => {
                 info!("CTRL_REG1_original: {}", data[0]);
             }
@@ -75,13 +63,12 @@ impl<'a, T: i2c::Instance> Lps22hh<'a, T> {
                 error!("I2C Error: {:?}", e);
             }
         }
-        self.i2c
+
+        i2c_mut
             .blocking_write(reg::I2C_SAD, &[Register::CTRL_REG1 as u8, reg1])
             .unwrap();
-        match self
-            .i2c
-            .blocking_write_read(reg::I2C_SAD, &[Register::CTRL_REG1 as u8], &mut data)
-        {
+
+        match i2c_mut.blocking_write_read(reg::I2C_SAD, &[Register::CTRL_REG1 as u8], &mut data) {
             Ok(()) => {
                 info!("CTRL_REG1_written: {}", data[0]);
             }
@@ -93,15 +80,12 @@ impl<'a, T: i2c::Instance> Lps22hh<'a, T> {
         let mut reg2: u8 = 0;
 
         // === CTRL_REG2 (10h) ===
-        reg2 |= IF_ADD_INC;
-        reg2 |= LOW_NOISE_EN;
+        reg2 |= reg::IF_ADD_INC;
+        reg2 |= reg::LOW_NOISE_EN;
         info!("CTRL_REG2 to write: {}", reg2);
 
         let mut data = [0u8, 1];
-        match self
-            .i2c
-            .blocking_write_read(reg::I2C_SAD, &[Register::CTRL_REG2 as u8], &mut data)
-        {
+        match i2c_mut.blocking_write_read(reg::I2C_SAD, &[Register::CTRL_REG2 as u8], &mut data) {
             Ok(()) => {
                 info!("CTRL_REG2_original: {}", data[0]);
             }
@@ -109,26 +93,30 @@ impl<'a, T: i2c::Instance> Lps22hh<'a, T> {
                 error!("I2C Error: {:?}", e);
             }
         }
-        self.i2c
+        i2c_mut
             .blocking_write(reg::I2C_SAD, &[Register::CTRL_REG2 as u8, reg2])
             .unwrap();
-        match self
-            .i2c
-            .blocking_write_read(reg::I2C_SAD, &[Register::CTRL_REG2 as u8], &mut data)
-        {
+        match i2c_mut.blocking_write_read(reg::I2C_SAD, &[Register::CTRL_REG2 as u8], &mut data) {
             Ok(()) => {
                 info!("CTRL_REG2_written: {}", data[0]);
+                Ok(true)
             }
             Err(e) => {
                 error!("I2C Error: {:?}", e);
+                Err(Error)
             }
         }
     }
 
-    pub fn sample(&mut self) {
+    pub async fn sample(&mut self) -> Result<bool, Error> {
         let mut buffer = [0u8; 3];
 
-        match self.i2c.blocking_write_read(
+        let mut i2c_unlocked = self.i2c.lock().await;
+
+        //let i2c_guard: &I2c<'static, peripherals::I2C1, NoDma, NoDma> = i2c_unlocked.as_ref().ok_or(Error)?;
+        let i2c_mut = i2c_unlocked.as_mut().ok_or(Error).unwrap(); // Lock the inner I2c instance
+
+        match i2c_mut.blocking_write_read(
             reg::I2C_SAD,
             &[Register::PRESSURE_OUT_XL as u8],
             &mut buffer,
@@ -148,20 +136,19 @@ impl<'a, T: i2c::Instance> Lps22hh<'a, T> {
 
         let mut buffer = [0u8; 2];
 
-        match self.i2c.blocking_write_read(
-            reg::I2C_SAD,
-            &[Register::TEMP_OUT_L as u8],
-            &mut buffer,
-        ) {
+        match i2c_mut.blocking_write_read(reg::I2C_SAD, &[Register::TEMP_OUT_L as u8], &mut buffer)
+        {
             Ok(()) => {
                 let temp_buf = (buffer[1] as i16) << 8 | buffer[0] as i16;
 
-                let temp = f32::from(temp_buf) * (reg::DEG_PER_LSB as f32) ;
+                let temp = f32::from(temp_buf) * (reg::DEG_PER_LSB as f32);
 
                 info!("Temperature: {}", temp);
+                Ok(true)
             }
             Err(e) => {
                 error!("I2C Error: {:?}", e);
+                Err(Error)
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,14 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_stm32::dma::NoDma;
 use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::i2c::I2c;
+use embassy_stm32::time::Hertz;
 use embassy_stm32::{bind_interrupts, i2c, peripherals};
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+use embassy_sync::mutex::Mutex;
+
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
 
@@ -18,28 +24,58 @@ bind_interrupts!(struct Irqs {
     I2C1_ER => i2c::ErrorInterruptHandler<peripherals::I2C1>;
 });
 
+type Channel1 =
+    Mutex<CriticalSectionRawMutex, Option<I2c<'static, peripherals::I2C1, NoDma, NoDma>>>;
+static I2C_REF: Channel1 = Mutex::new(None);
+
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
     let mut led = Output::new(p.PB7, Level::Low, Speed::Low);
+    let i2c_ch: I2c<'static, peripherals::I2C1, NoDma, NoDma> = i2c::I2c::new(
+        p.I2C1,
+        p.PB8,
+        p.PB9,
+        Irqs,
+        NoDma,
+        NoDma,
+        Hertz(100_000),
+        i2c::Config::default(),
+    );
 
-    let mut sensor = Lps22hh::new(p.I2C1, p.PB8, p.PB9, Irqs);
+    {
+        *(I2C_REF.lock().await) = Some(i2c_ch);
+    }
+    let mut sensor = Lps22hh::new(&I2C_REF);
 
-    
-        info!("Blink");
-        Timer::after(Duration::from_millis(1000)).await;
-        if sensor.check_device_id() {
-            led.set_high();
-        }
-        Timer::after(Duration::from_millis(1000)).await;
-        sensor.apply_config();
-        loop {
-            Timer::after(Duration::from_millis(100)).await;
-            sensor.sample();
-            led.set_low();
-            Timer::after(Duration::from_millis(100)).await;
-            led.set_high();
-        }
+    let mut sensor2 = Lis2mdl::new(&I2C_REF);
+    //let mut sensor = Lps22hh::new(p.I2C1, p.PB8, p.PB9, Irqs);
 
-        
+    info!("Blink");
+    Timer::after(Duration::from_millis(1000)).await;
+    if sensor.check_device_id().await.unwrap() {
+        info!("Polling Lps22hh");
+        led.set_high();
+    }
+    Timer::after(Duration::from_millis(1000)).await;
+    sensor.apply_config().await.unwrap();
+    led.set_low();
+
+    Timer::after(Duration::from_millis(1000)).await;
+    if sensor2.check_device_id().await.unwrap() {
+        info!("Polling Lis2mdl");
+        led.set_high();
+    }
+    Timer::after(Duration::from_millis(1000)).await;
+    sensor2.apply_config().await.unwrap();
+
+    loop {
+        Timer::after(Duration::from_millis(400)).await;
+        led.set_low();
+        sensor.sample().await.unwrap();
+        Timer::after(Duration::from_millis(10)).await;
+        led.set_high();
+        sensor2.sample().await.unwrap();
+        Timer::after(Duration::from_millis(400)).await;
+    }
 }
